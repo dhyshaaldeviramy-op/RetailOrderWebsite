54d39336d4f232103bef98b25ce5c401e0ce23e5
aravindhan14v <aravindhan142005@gmail.com>
2026-04-13 20:34:15 +0530

    retail backend

--- RetailOrderWebsite/Models/CartItem.cs
+++ RetailOrderWebsite/Models/CartItem.cs
@@ -8,5 +8,6 @@ namespace RetailOrderWebsite.Models
         public int Id { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
+    //    public Product Product { get; set; }
     }
 }
--- RetailOrderWebsite/Program.cs
+++ RetailOrderWebsite/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using RetailOrderWebsite.Data;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -11,12 +13,28 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var allowedOrigins = builder.Configuration
+    .GetSection("AllowedOrigins")
+    .Get<string[]>() ?? new[] { "http://localhost:4200" };
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins(allowedOrigins)
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AllowFrontend");
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
